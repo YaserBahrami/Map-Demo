811b6d4f20f3c695e840a1cae0c523ec5dceaf4d
yaserbahrami <yaserbahrami@outlook.com>
2016-05-03 16:44:27 +0430

    just commit
    very simple and demo!
    holy shit! this is shit :|

--- Map Demo/Map Demo/ViewController.swift
+++ Map Demo/Map Demo/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController,MKMapViewDelegate {
+class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
 
     @IBOutlet var mapKit: MKMapView!
     
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
         
         let latitude:CLLocationDegrees = 35.740434
         let longitude:CLLocationDegrees = 51.447037
@@ -29,13 +38,66 @@ class ViewController: UIViewController,MKMapViewDelegate {
         
         mapKit.setRegion(region, animated: true)
         
-
-
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "My Location"
+        annotation.subtitle = "test 4 test"
         
+        mapKit.addAnnotation(annotation)
         
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.action(_:)))
+        uilpgr.minimumPressDuration = 2
         
+        mapKit.addGestureRecognizer(uilpgr)
         
     }
+    
+    func action(gestureRecognizer:UIGestureRecognizer){
+        let touchPoint = gestureRecognizer.locationInView(self.mapKit)
+        
+        let newCoordinate:CLLocationCoordinate2D = mapKit.convertPoint(touchPoint, toCoordinateFromView: self.mapKit)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinate
+        annotation.title = "My Second Location"
+        annotation.subtitle = "just 4 test"
+        
+        mapKit.addAnnotation(annotation)
+
+    }
+    
+    
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //print(locations)
+        let userLocation : CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        
+        
+        let locationPoint : CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let latDelta:CLLocationDegrees = 0.01
+        let longDelta:CLLocationDegrees = 0.01
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(locationPoint, span)
+        self.mapKit.setRegion(region, animated: false)
+        
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = ""
+        annotation.subtitle=""
+        annotation.coordinate = locationPoint
+        self.mapKit.addAnnotation(annotation)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
